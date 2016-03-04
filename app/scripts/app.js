f12d2f170fc9276eac81f85879f2fe543f814653
Lucien Bénié <lucien.benie@gmail.com>
2016-03-04 14:18:51 -0500

    using angular-elastic-grid

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,7 +5,8 @@ angular
     'ngCookies',
     'ngResource',
     'ngSanitize',
-    'ngRoute'
+    'ngRoute',
+    'angular-elastic-grid'
   ])
   .config(function($routeProvider) {
     $routeProvider
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,16 +1,66 @@
 'use strict';
 
+function setupData(value) {
+  var item = {};
+
+  function description() {
+    item.description += '<br/><br/>';
+
+    angular.isDefined(item.country) ?
+    item.description += '<i class="fa fa-globe fa-lg"/>' + ' ' + item.country  + '<br/>' : item.description += '';
+
+    angular.isDefined(item.region) ?
+    item.description += '<i class="fa fa-map-marker fa-lg"/>' + ' ' + item.region + '<br/>' : item.description += '';
+
+    angular.isDefined(item.temperature) ?
+    item.description += '<i class="fa fa-glass fa-lg"/>' + ' ' + item.temperature + '<br/>' : item.description += '';
+
+    angular.isDefined(item.producer) ?
+    item.description += '<i class="fa fa-user fa-lg"/>' + ' ' + item.producer + '<br/>' : item.description += '';
+
+    angular.isDefined(item.price) ?
+    item.description += '<i class="fa fa-money fa-lg"/>' + ' ' + item.price + '<br/>' : item.description += '';
+
+    return item.description;
+  }
+
+
+  item.title = value.tpnomdebouteille;
+  item.price = value.tpprixnormal;
+  item.color = value.tpcouleur;
+  item.region = value.tpregion;
+  item.country = value.tppays;
+  item.producer = value.tpproducteur;
+
+  item.description = angular.isDefined(value.tpcompagniedescription) ?
+                     item.description = value.tpcompagniedescription : '';
+
+  item.description = description();
+
+  item.thumbnail = [value.tpthumbnailuri];
+  item.large = [value.tpthumbnailuri];
+  item.temperature = value.tptemperaturedeservice;
+  item.taste = value.tppastilledegout;
+
+  item.button_list = [{'title': 'En savoir plus', 'url': value.sysclickableuri}];
+  item.tags = value.tpcepagenomsplitgroup.split(';');
+
+  return item;
+}
+
+
+
 angular.module('coveoFrontendChallengeApp')
   .controller('MainCtrl', function($scope, api) {
-    $scope.isCollapse = true;
     $scope.wines = [];
+    $scope.elastic = [];
+
     api.wineByType({type: 'Merlot'}).then(function(result) {
-      console.log(result.data.results);
-      $scope.wines = result.data.results;
+      angular.forEach(result.data.results, function(value) {
+        $scope.wines.push(value.raw);
+        $scope.elastic.push(setupData(value.raw));
+        $('#loader').hide();
+        $('#wineList').show();
+      });
     });
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
   });
